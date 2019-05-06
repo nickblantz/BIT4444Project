578925557b2a9565bf6f35565808f6dce09f893f
Nick Blantz <nicholasblantz@gmail.com>
2019-05-06 10:58:13 -0400

    crediting yelp

--- Resources/template/footer.php
+++ Resources/template/footer.php
@@ -4,7 +4,7 @@ function generate_footer() {
 	echo "<div class='row h-100'>";
 	echo "<div class='col-sm-2 d-none d-sm-block'></div>";
 	echo "<div class='col-sm-8 col-6 center-content'>Blantz - Mateen - Yeh</div>";
-	echo "<div class='col-sm-2 col-6 justify-right'>Legal</div>";
+	echo "<div class='col-sm-2 col-6 justify-right'><span class='align-middle'>Powered by&nbsp;</span><img height=30 src='" . redirect_prefix('Resources/images/yelp-logo.png') . "' /></div>";
 	echo "</div></footer>";
 }
 ?>
